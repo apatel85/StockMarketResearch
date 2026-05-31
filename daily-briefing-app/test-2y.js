b106ca7a3e84bf7d7dd7ccd6d8928d12b9b211c3
apatel85 <apatel85@gmail.com>
2026-05-31 12:50:39 -0400

    Delete daily-briefing-app directory

--- daily-briefing-app/test-2y.js
+++
@@ -1,4 +0,0 @@
-const YahooFinance = require('yahoo-finance2').default;
-const yahooFinance = new YahooFinance();
-yahooFinance.quote('ZT=F').then(console.log).catch(console.error);
-yahooFinance.quote('^IRX').then(console.log).catch(console.error);
--- daily-briefing-app/test-yf.js
+++
@@ -1,3 +0,0 @@
-const YahooFinance = require('yahoo-finance2').default;
-const yahooFinance = new YahooFinance();
-yahooFinance.quote('AAPL').then(console.log).catch(console.error);
